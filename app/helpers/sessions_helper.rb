681e9393bee4c53323c924e0c35dd47a0ca7ecb0
YJ Cho <reverince@gmail.com>
2018-10-25 15:39:17 +0000

    Implement login remembering

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,9 +4,23 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remember user in permanent cookie
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Return current user or nil
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find(user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find(user_id)
+      if user && user.remembered?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -14,7 +28,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
