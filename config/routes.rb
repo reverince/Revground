49ff51d1fa877bfc6456a52fbac6622bd6a3a1ac
YJ Cho <reverince@gmail.com>
2018-10-25 12:44:35 +0000

    Implement user login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
+
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
